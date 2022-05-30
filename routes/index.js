379d9cc7a3a0b50fe40f9d0197a65e188c341a7c
Cheaguz <aguzf83@gmail.com>
2022-05-30 10:05:39 -0300

    merge 1-user_client-create-menu-model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const menuRouter = require('./menuRoutes')
+
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/menu',menuRouter)
 
 module.exports = router;
